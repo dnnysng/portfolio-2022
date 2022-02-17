6aee1500ffa7fbfae18af9a93870129b233781f7
dseng13 <dseng13@gmail.com>
2022-02-17 11:58:29 -0500

    fixed animate issue

--- src/pages/home.js
+++ src/pages/home.js
@@ -3,12 +3,14 @@ import Hero from '../components/hero/Hero'
 import style from './page.module.css'
 import theme from '../App.module.css'
 import FeaturedWork from '../components/featuredWork/featuredWork'
+import LatestUpdates from '../components/latestUpdates/latestUpdates'
 
 const home = (props) => {
     return (
         <div className={`${props.menu ? style.wrapperDown : style.wrapper} ${props.dark ? theme.darkTheme : theme.lightTheme}`}>
             <Hero />
             <FeaturedWork />
+            <LatestUpdates />
         </div>
     )
 }
