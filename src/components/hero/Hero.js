76882c7a1b4ae4137344736d14b3000a15da7f28
dseng13 <dseng13@gmail.com>
2022-02-23 23:30:13 -0500

    finished cleaning hero section code

--- src/components/hero/Hero.js
+++ src/components/hero/Hero.js
@@ -6,16 +6,18 @@ import Button from '../button/Button'
 
 const Hero = (props) => {
     return (
-        <div className={style.hero}>
+        <section className={style.block}>
             <div className={style.wrapper}>
                 <div className={style.content}>
                     <h1 className={style.heading}>Bringing your visions to life, one pixel at a time.</h1>
-                    <p className={style.paragraph}>I'm an experienced <strong>Frontend Developer</strong> ready to work on <em>your projects</em>.</p>
+                    <p className={style.paragraph}>I'm a <strong>front-end web developer</strong> specializing in pixel-perfect, responsive web design. My framework of choice is React.js, however, I have a solid foundation in HTML, CSS, and JavaScript.</p>
+                    <p className={style.paragraph}><em>I'm ready to make an impact on your projects.</em></p>
                     <Button
                         label="Email me at danny.s.seng@gmail.com"
                         href="mailto:danny.s.seng@gmail.com"
                         variant="cta"
                         dark={props.dark}
+                        className={style.button}
                     />
                 </div>
                 <div className={style.photoWrapper}>
@@ -23,7 +25,7 @@ const Hero = (props) => {
                     <img className={style.photo} src={photo} alt="Portrait of Danny Seng" />
                 </div>
             </div>
-        </div>
+        </section>
     )
 }
 
