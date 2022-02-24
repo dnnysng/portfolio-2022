5402feb1368ab5efbe2468c07dcf89f566f56660
dseng13 <dseng13@gmail.com>
2022-02-24 00:31:41 -0500

    finished cleaning work and workcards

--- src/components/work/workCard/workCard.js
+++ src/components/work/workCard/workCard.js
@@ -4,18 +4,16 @@ import Button from '../../button/Button'
 const workCard = (props) => {
   return (
     <div className={style.wrapper}>
-      <div className={style.projectWrapper}>
-        <img
-          src={props.image}
-          alt={`"${style.title}" project thumbnail`}
-          className={style.thumbnail}
-        />
-        <div className={style.content}>
-          <h2 className={style.title}>{props.title}</h2>
-          <p className={style.description}>{props.description}</p>
-          <Button className={style.button} variant="cta" label="View Live" href={props.live} />
-          {props.github && <Button className={style.button} variant="secondary" label="Github" href={props.github} />}
-        </div>
+      <img
+        src={props.image}
+        alt={`"${style.title}" project thumbnail`}
+        className={style.thumbnail}
+      />
+      <div className={style.content}>
+        <h2 className={style.title}>{props.title}</h2>
+        <p className={style.description}>{props.description}</p>
+        <Button className={style.button} variant="cta" label="View Live" href={props.live} />
+        {props.github && <Button className={style.button} variant="secondary" label="Github" href={props.github} />}
       </div>
     </div>
   )
