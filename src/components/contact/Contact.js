09832d5018becd15588c5b8c4484f95c72fba10f
dseng13 <dseng13@gmail.com>
2022-02-24 01:38:02 -0500

    cleaned contact code

--- src/components/contact/Contact.js
+++ src/components/contact/Contact.js
@@ -4,13 +4,13 @@ import Button from '../button/Button'
 
 const Contact = () => {
   return (
-    <div id="contact" className={style.contact}>
+    <section id="contact" className={style.block}>
       <div className={style.wrapper}>
         <h1 className={style.title}>Contact Me</h1>
         <p className={style.paragraph}>I'm an experienced <strong>frontend developer</strong> available for hire. Let's talk!</p>
         <Button variant="cta" label="Email me at danny.s.seng@gmail.com" href="mailto:danny.s.seng@gmail.com" />
       </div>
-    </div>
+    </section>
   )
 }
 
