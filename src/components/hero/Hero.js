87285e0f96919b04732c84dd6e5da01554f04686
dseng13 <dseng13@gmail.com>
2022-02-27 00:32:19 -0500

    updated hero section

--- src/components/hero/Hero.js
+++ src/components/hero/Hero.js
@@ -1,7 +1,5 @@
 import React from 'react'
 import style from './Hero.module.css'
-import cool from '../../assets/videos/cool.mp4'
-import photo from '../../assets/images/cartoon-danny-min.png'
 import Button from '../button/Button'
 
 const Hero = (props) => {
@@ -10,7 +8,7 @@ const Hero = (props) => {
             <div className={style.wrapper}>
                 <div className={style.content}>
                     <h1 className={style.heading}>Bringing your visions to life, one pixel at a time.</h1>
-                    <p className={style.paragraph}>I'm a <strong>front-end web developer</strong> specializing in pixel-perfect, responsive web design. My framework of choice is React.js, however, I have a solid foundation in HTML, CSS, and JavaScript.</p>
+                    <p className={style.paragraph}>I'm a <strong>front-end web developer</strong> specializing in pixel-perfect, responsive web design. My framework of choice is React.js</p>
                     <p className={style.paragraph}><em>I'm ready to make an impact on your projects.</em></p>
                     <Button
                         label="Email me at danny.s.seng@gmail.com"
@@ -19,10 +17,6 @@ const Hero = (props) => {
                         dark={props.dark}
                         className={style.button}
                     />
-                </div>
-                <div className={style.photoWrapper}>
-                    <video loop muted autoPlay type="video/mp4" className={style.video} src={cool} alt="Animated purple background" />
-                    <img className={style.photo} src={photo} alt="Portrait of Danny Seng" />
                 </div>
             </div>
         </section>
