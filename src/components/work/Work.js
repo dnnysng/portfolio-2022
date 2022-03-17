77b06501431512178d50f8370642739891a33a25
dseng13 <dseng13@gmail.com>
2022-03-16 21:38:34 -0400

    updated mockups to higher quality

--- src/components/work/Work.js
+++ src/components/work/Work.js
@@ -6,9 +6,9 @@ import Card from './workCard/workCard'
 
 import quicktrailer from '../../assets/images/quicktrailers-mockup-min.png'
 import afexpress from '../../assets/images/afexpress-mockup-min.png'
-import dualsense from '../../assets/images/controller-mockup.png'
-import vl from '../../assets/images/vl-mockup.png'
-import liftYourself from '../../assets/images/lift-yourself-mockup.png'
+// import dualsense from '../../assets/images/controller-mockup.png'
+import vl from '../../assets/images/vl-mockup-min.png'
+// import liftYourself from '../../assets/images/lift-yourself-mockup.png'
 
 const Work = () => {
 
@@ -18,7 +18,21 @@ const Work = () => {
       "title": "AF Express NFT",
       "description": "Non-fungible, but oh so delicious. I created an NFT collection of popular asian foods and this website displays each dish in all its glory. Now listed on Magic Eden, the most popular Solana NFT marketplace.",
       "live": "https://www.afexpress.art",
-      "tech": ["React", "Crypto", "Solana", "NFT"]
+      "tech": [
+        "React",
+        "Router",
+        "Framer Motion",
+        "Crypto",
+        "Solana",
+        "NFT"]
+    },
+    {
+      "image": vl,
+      "title": "Virtual Learning Online",
+      "tech": ["WordPress", "LearnDash LMS", "Woocommerce", "Elementor Pro"],
+      "description": "I learned a lot during this project and was the lead in both design and development. Main features include a learning management system, a group dashboard, Woocommerce & CRM integration.",
+      "live": "https://www.virtuallearnonline.com",
+      "code": "https://codepen.io/dnnysng/pen/zYwpgpL",
     },
     {
       "image": quicktrailer,
@@ -28,31 +42,23 @@ const Work = () => {
       "github": "https://github.com/dnnysng/quick-trailers",
       "tech": ["React", "Rest API", "TheMovieDB"]
     },
-    {
-      "image": vl,
-      "title": "Virtual Learning Online",
-      "tech": ["WordPress", "LearnDash LMS", "Woocommerce", "Elementor Pro"],
-      "description": "I learned a lot during this project and was the lead in both design and development. Main features include a learning management system, a group dashboard, Woocommerce & CRM integration.",
-      "live": "https://www.virtuallearning.com",
-      "code": "https://codepen.io/dnnysng/pen/zYwpgpL",
-    },
-    {
-      "image": liftYourself,
-      "title": "Lift Yourself: Calisthenics",
-      "tech": ["React", "DayJS"],
-      "description": "Lift Yourself takes the complexity out of tracking your calisthenics workouts. Simply choose your workout type and track what's important... the sets and reps!",
-      "live": "https://www.liftyourself.app",
-      "github": "https://github.com/dnnysng/lift-yourself",
-      "cardVariant": "mobileThumbnail"
-    },
-    {
-      "image": dualsense,
-      "title": "DualSense Wireless Controller",
-      "description": "This fullscreen featured product page showcases the Dualsense Wireless Controller and it's variety of new colors.",
-      "live": "https://dazzling-villani-5961ae.netlify.app/",
-      "github": "https://github.com/dnnysng/fullscreen-featured-product",
-      "tech": ["React", "Figma"]
-    },
+    // {
+    //   "image": liftYourself,
+    //   "title": "Lift Yourself: Calisthenics",
+    //   "tech": ["React", "DayJS"],
+    //   "description": "Lift Yourself takes the complexity out of tracking your calisthenics workouts. Simply choose your workout type and track what's important... the sets and reps!",
+    //   "live": "https://www.liftyourself.app",
+    //   "github": "https://github.com/dnnysng/lift-yourself",
+    //   "cardVariant": "mobileThumbnail"
+    // },
+    // {
+    //   "image": dualsense,
+    //   "title": "DualSense Wireless Controller",
+    //   "description": "This fullscreen featured product page showcases the Dualsense Wireless Controller and it's variety of new colors.",
+    //   "live": "https://dazzling-villani-5961ae.netlify.app/",
+    //   "github": "https://github.com/dnnysng/fullscreen-featured-product",
+    //   "tech": ["React", "Figma"]
+    // },
   ]
 
   return (
