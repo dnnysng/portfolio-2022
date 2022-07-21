bd1620852abc82c23440547155f8592e9391f059
dseng13 <dseng13@gmail.com>
2022-07-20 21:15:23 -0400

    shortened the loader time

--- src/App.js
+++ src/App.js
@@ -1,46 +1,49 @@
-import { useState, useEffect } from 'react'
+import { useState, useEffect } from 'react';
 
-import LoadSpinner from './components/loadSpinner/LoadSpinner'
+import LoadSpinner from './components/loadSpinner/LoadSpinner';
 
-import Header from './components/header/Header'
-import Home from './pages/Home'
-import Footer from './components/footer/Footer'
+import Header from './components/header/Header';
+import Home from './pages/Home';
+import Footer from './components/footer/Footer';
 
-import style from './App.module.css'
+import style from './App.module.css';
 
 export default function App() {
+    const [isDark, setIsDark] = useState(false);
+    const [isMenuOpen, setIsMenuOpen] = useState(false);
+    const [loading, setLoading] = useState(true);
 
-  const [isDark, setIsDark] = useState(false);
-  const [isMenuOpen, setIsMenuOpen] = useState(false);
-  const [loading, setLoading] = useState(true);
+    // get local preference for theme
+    useEffect(() => {
+        const rawData = localStorage.getItem('isDark');
+        if (rawData) setIsDark(JSON.parse(rawData));
+    }, []);
 
-  // get local preference for theme
-  useEffect(() => {
-    const rawData = localStorage.getItem("isDark");
-    if (rawData) setIsDark(JSON.parse(rawData))
-  }, [])
+    // set local preference for theme on theme change
+    useEffect(() => {
+        localStorage.setItem('isDark', JSON.stringify(isDark));
+    }, [isDark]);
 
-  // set local preference for theme on theme change
-  useEffect(() => {
-    localStorage.setItem("isDark", JSON.stringify(isDark))
-  }, [isDark])
+    // time for loader animation to run
+    setTimeout(() => {
+        setLoading(false);
+    }, 2500);
 
-  // time for loader animation to run
-  setTimeout(() => {
-    setLoading(false);
-  }, 4000)
-
-  return (
-    <div className={`${style.container} ${isDark ? "dark" : "light"} ${loading && style.noScroll}`}>
-      {loading && <LoadSpinner />}
-      <Header
-        isMenuOpen={isMenuOpen}
-        setIsMenuOpen={setIsMenuOpen}
-        isDark={isDark}
-        setIsDark={setIsDark}
-      />
-      <Home menu={isMenuOpen} />
-      <Footer />
-    </div>
-  );
+    return (
+        <div
+            className={`${style.container} ${isDark ? 'dark' : 'light'} ${
+                loading && style.noScroll
+            }`}
+        >
+            {loading && <LoadSpinner />}
+            <Header
+                isMenuOpen={isMenuOpen}
+                setIsMenuOpen={setIsMenuOpen}
+                isDark={isDark}
+                setIsDark={setIsDark}
+            />
+            <Home menu={isMenuOpen} />
+            <Footer />
+        </div>
+    );
 }
