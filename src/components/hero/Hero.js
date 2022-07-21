2fd8fe67947fb89cbfb75847c496ff2f9a22f32c
dseng13 <dseng13@gmail.com>
2022-07-20 21:06:09 -0400

    updated

--- src/components/hero/Hero.js
+++ src/components/hero/Hero.js
@@ -1,28 +1,30 @@
-import LinkButton from '../link-button/LinkButton'
+import LinkButton from '../link-button/LinkButton';
 
-import style from './Hero.module.css'
+import style from './Hero.module.css';
 
 export default function Hero() {
-  return (
-    <section className={style.block}>
-      <p id="hero" className={style.anchor}></p>
-      <div className={style.container}>
-        <div className={style.content}>
-          <h1 className={style.heading}>
-            Bringing your visions to life, one pixel at a time.
-          </h1>
-          <p className={style.paragraph}>
-            I'm an experienced <strong>Front-end Web Developer</strong> with a demonstrated history of working in the information technology and services industry. I specialize in pixel-perfect, responsive web design.
-          </p>
-          <LinkButton
-            label="Email me at danny.s.seng@gmail.com"
-            href="#contact"
-            variant="cta"
-            className={style.button}
-            contact={true}
-          />
-        </div>
-      </div>
-    </section>
-  )
+    return (
+        <section className={style.block}>
+            <p id="hero" className={style.anchor}></p>
+            <div className={style.container}>
+                <div className={style.content}>
+                    <h1 className={style.heading}>
+                        Bringing your visions to life, one pixel at a time.
+                    </h1>
+                    <p className={style.paragraph}>
+                        I'm an experienced{' '}
+                        <strong>Front-end Designer & Developer</strong>{' '}
+                        specializing in pixel-perfect websites and components.
+                    </p>
+                    <LinkButton
+                        label="Email me at danny.s.seng@gmail.com"
+                        href="#contact"
+                        variant="cta"
+                        className={style.button}
+                        contact={true}
+                    />
+                </div>
+            </div>
+        </section>
+    );
 }
