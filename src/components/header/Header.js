bc2c7fac975f9876fcab676660784a2497102bdd
dseng13 <dseng13@gmail.com>
2022-08-28 11:38:19 -0400

    removed option to change theme

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,38 +1,45 @@
-import { AnimatePresence } from 'framer-motion'
+import { AnimatePresence } from 'framer-motion';
 
-import MobileMenu from './mobile-menu/MobileMenu'
-import { ReactComponent as Hamburger } from '../../assets/icons/hamburger.svg'
-import { ReactComponent as Sun } from '../../assets/icons/sun-shape.svg'
+import MobileMenu from './mobile-menu/MobileMenu';
+import { ReactComponent as Hamburger } from '../../assets/icons/hamburger.svg';
 
-import style from './Header.module.css'
+import style from './Header.module.css';
 
-const Header = ({ isMenuOpen, setIsMenuOpen, isDark, setIsDark }) => {
-
+const Header = ({ isMenuOpen, setIsMenuOpen }) => {
     // menu toggle
     function toggleMenu() {
-        setIsMenuOpen(!isMenuOpen)
+        setIsMenuOpen(!isMenuOpen);
     }
 
     return (
         <header className={style.block}>
             <div className={style.container}>
                 <div className={style.logoWrapper}>
-                    <a href="#hero"><h3 className={style.logo}>Danny Seng</h3></a>
+                    <a href="#hero">
+                        <h3 className={style.logo}>Danny Seng</h3>
+                    </a>
                 </div>
                 <nav className={style.nav}>
-                    <a className={style.link} href='#hero'>Home</a>
-                    <a className={style.link} href='#work'>Work</a>
-                    <a className={style.link} href='#latestUpdates'>Blog</a>
-                    <a className={style.link} href='#contact'>Contact</a>
+                    <a className={style.link} href="#hero">
+                        Home
+                    </a>
+                    <a className={style.link} href="#work">
+                        Work
+                    </a>
+                    <a className={style.link} href="#latestUpdates">
+                        Blog
+                    </a>
+                    <a className={style.link} href="#contact">
+                        Contact
+                    </a>
                 </nav>
-                <Sun className={style.sun} onClick={() => setIsDark(!isDark)} />
                 <AnimatePresence>
                     {isMenuOpen && <MobileMenu toggleMenu={toggleMenu} />}
                 </AnimatePresence>
                 <Hamburger className={style.hamburger} onClick={toggleMenu} />
             </div>
         </header>
-    )
-}
+    );
+};
 
-export default Header
+export default Header;
