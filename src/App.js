16fab8e57c06b2ff2cf485e38de35639b5037c54
dseng13 <dseng13@gmail.com>
2022-08-28 11:37:09 -0400

    removed loader

--- src/App.js
+++ src/App.js
@@ -1,6 +1,4 @@
-import { useState, useEffect } from 'react';
-
-import LoadSpinner from './components/loadSpinner/LoadSpinner';
+import { useState } from 'react';
 
 import Header from './components/header/Header';
 import Home from './pages/Home';
@@ -9,39 +7,11 @@ import Footer from './components/footer/Footer';
 import style from './App.module.css';
 
 export default function App() {
-    const [isDark, setIsDark] = useState(false);
     const [isMenuOpen, setIsMenuOpen] = useState(false);
-    const [loading, setLoading] = useState(true);
-
-    // get local preference for theme
-    useEffect(() => {
-        const rawData = localStorage.getItem('isDark');
-        if (rawData) setIsDark(JSON.parse(rawData));
-    }, []);
-
-    // set local preference for theme on theme change
-    useEffect(() => {
-        localStorage.setItem('isDark', JSON.stringify(isDark));
-    }, [isDark]);
-
-    // time for loader animation to run
-    setTimeout(() => {
-        setLoading(false);
-    }, 2500);
 
     return (
-        <div
-            className={`${style.container} ${isDark ? 'dark' : 'light'} ${
-                loading && style.noScroll
-            }`}
-        >
-            {loading && <LoadSpinner />}
-            <Header
-                isMenuOpen={isMenuOpen}
-                setIsMenuOpen={setIsMenuOpen}
-                isDark={isDark}
-                setIsDark={setIsDark}
-            />
+        <div className={`${style.container} ${'theme'}`}>
+            <Header isMenuOpen={isMenuOpen} setIsMenuOpen={setIsMenuOpen} />
             <Home menu={isMenuOpen} />
             <Footer />
         </div>
