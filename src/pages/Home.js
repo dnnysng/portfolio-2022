880dda59a7e4d9028be4f31994eb403b890df0b2
dseng13 <dseng13@gmail.com>
2022-08-28 11:43:18 -0400

    removed hero section

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,17 +1,15 @@
-import Hero from '../components/hero/Hero'
-import Work from '../components/work/Work'
-import Blog from '../components/blog/Blog'
-import Contact from '../components/contact/Contact'
+import Work from '../components/work/Work';
+import Blog from '../components/blog/Blog';
+import Contact from '../components/contact/Contact';
 
-import style from './Home.module.css'
+import style from './Home.module.css';
 
 export default function Home() {
-  return (
-    <div className={style.base}>
-      <Hero />
-      <Work />
-      <Blog />
-      <Contact />
-    </div>
-  )
+    return (
+        <div className={style.base}>
+            <Work />
+            <Blog />
+            <Contact />
+        </div>
+    );
 }
